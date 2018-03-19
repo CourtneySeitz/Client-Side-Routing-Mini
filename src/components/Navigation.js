9b8eed2cb0d81b0fbbc0520241124902824dbba7
Courtney Seitz <courtneyelizabethseitz@gmail.com>
2018-03-18 22:52:08 -0400

    initial commit practice

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import { Nav, NavItem, NavLink } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
     <div>
       <Nav className="App">
         <h1>React Router Mini</h1>
+        <NavLink>
         <NavItem>
-          <NavLink href="#">Home</NavLink>
+          <Link to="/">Home</Link>
         </NavItem>
+        </NavLink>
+        <NavLink>
         <NavItem>
-          <NavLink href="#">About</NavLink>
+          <Link to="/about">About</Link>
         </NavItem>
+        </NavLink>
+        <NavLink>
         <NavItem>
-          <NavLink href="#">Contact</NavLink>
+          <Link to="/contact">Contact</Link>
         </NavItem>
+        </NavLink>
       </Nav>
     </div>
   );
